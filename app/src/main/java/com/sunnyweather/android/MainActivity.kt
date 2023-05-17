d3b6535a1c997c1bae85d7a26b2b899d4d015e2e
dylanli <dylanli190815@gmail.com>
2023-05-17 13:44:43 +0800

    实现搜索全球城市数据的功能

--- app/src/main/java/com/sunnyweather/android/MainActivity.kt
+++ app/src/main/java/com/sunnyweather/android/MainActivity.kt
@@ -2,10 +2,16 @@ package com.sunnyweather.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.liveData
+import kotlinx.coroutines.Dispatchers
 
 class MainActivity : AppCompatActivity() {
+    private  val TAG = "lwt"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(TAG, "onCreate: ")
     }
+
 }
---
+++ app/src/main/java/com/sunnyweather/android/logic/Repository.kt
@@ -0,0 +1,28 @@
+package com.sunnyweather.android.logic
+
+import android.util.Log
+import androidx.lifecycle.liveData
+import com.sunnyweather.android.logic.model.Place
+import com.sunnyweather.android.logic.network.SunnyWeatherNetwork
+import kotlinx.coroutines.Dispatchers
+import java.lang.Exception
+
+object Repository {
+    val TAG="lwt"
+    fun searchPlaces(query: String) = liveData(Dispatchers.IO) {
+        val result = try {
+            val placeResponse = SunnyWeatherNetwork.searchPlaces(query)
+            if (placeResponse.status == "ok") {
+                val places = placeResponse.places
+                Log.d(TAG, "searchPlaces: $places")
+                Result.success(places)
+            } else {
+                Log.d(TAG, "status == noOK")
+                Result.failure(RuntimeException("response status is ${placeResponse.status}"))
+            }
+        } catch (e: Exception) {
+            Result.failure<List<Place>>(e)
+        }
+        emit(result)
+    }
+}
